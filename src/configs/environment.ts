63fe955abd4db079d451530498b7c9d1a11fbc64
san2002chowdhury <sunilkrchowdhury66@gmail.com>
2024-10-26 18:25:36 +0530

    db commit

--- src/configs/environment.ts
+++ src/configs/environment.ts
@@ -1,10 +1,9 @@
 export const EnvConfig = {
-    server: {
-        port: Number.parseInt(process.env.PORT, 10) || 8080,
-        environment: process.env.ENV || "PROD",
-    },
-    database: {
-        name: process.env.DB_NAME,
-        host: process.env.DB_HOST
-    }
-}
+  server: {
+    port: Number.parseInt(process.env.PORT, 10) || 8080,
+    environment: process.env.ENV || "PROD",
+  },
+  database: {
+    url: process.env.DB_URL,
+  },
+};
